63625cf79a3c6b4fd16d9fe866307f39c9c09266
Johannes Schmitt <Johannesschmitt92@gmail.com>
2022-03-04 19:25:07 +0100

    Angular:
    added TokenGuard to check further routing if token is set or not

---
+++ frontend/src/modules/shared/token-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {TokenService} from "./token.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenGuardService implements CanActivate{
+
+  constructor(private tokenService: TokenService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if(this.tokenService.getToken())
+    {return true}
+    else
+    {
+      this.router.navigate(["no-token"])
+      return false
+    }
+  }
+
+
+}
