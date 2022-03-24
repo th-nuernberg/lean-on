49ecdd2b2bca2adba3653840f0b97c4d9cef1532
johan <johannes-schmitt82@gmx.de>
2022-03-24 18:32:31 +0100

    Angular:
    Began setting up frame for bmc main page

--- frontend/src/modules/bmc/bmc/bmc.component.ts
+++ frontend/src/modules/bmc/bmc/bmc.component.ts
@@ -13,6 +13,16 @@ import {NewHypothesisDialogComponent} from "../new-hypothesis-dialog/new-hypothe
 })
 export class BMCComponent implements OnInit {
 
+  categories = [{text:'Key Partners ', col:'2', row: '2'},
+    {text:'Key Activities', col:'2', row: '1'},
+    {text:'Value Propositions', col:'2', row: '2'},
+    {text:'Customer Relationships', col:'2', row: '1'},
+    {text:'Customer Segments', col:'2', row: '2'},
+    {text:'Key Resources', col:'2', row: '1'},
+    {text:'Channels', col:'2', row: '1'},
+    {text:'Cost Structure', col:'5', row: '1'},
+    {text:'Revenue Streams', col:'5', row: '1'},]
+
   constructor(private tokenService: TokenService, private hypothesesDataService: HypothesesDataService,
               public dialog: MatDialog) { }
 
