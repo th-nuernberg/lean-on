4af088fe4b238e3a8a21370ee6d053dfb0c5b202
johan <johannes-schmitt82@gmx.de>
2022-03-31 18:29:07 +0200

    Backend:
    Started Initialize Database_counter Database

--- backend/source/database/set-up-team-counter.ts
+++ backend/source/database/set-up-team-counter.ts
@@ -1,12 +1,31 @@
-import {getAllUserDatabaseNames} from "./getAllUserDatabaseNames";
+import {getAllDatabaseNames, getAllUserDatabaseNames} from "./getAllUserDatabaseNames";
 import {database} from "./database";
 
 export async function setUpTeamCounter(){
 
-    let checkForDatabaseCounter
+    let databaseNames = await getAllDatabaseNames()
+    let checkForDatabaseCounter = await checkForTeamCounter(databaseNames)
+
+    if(checkForDatabaseCounter)
+    {
+        return true
+    }
+    else
+    {
+        
+    }
 
 }
 
-async function checkForTeamCounter()
+async function checkForTeamCounter(databaseNames: string[])
 {
+    for(let databaseName of databaseNames)
+    {
+        if(databaseName === "database_counter")
+        {
+            return true
+        }
+    }
+
+    return false
 }
